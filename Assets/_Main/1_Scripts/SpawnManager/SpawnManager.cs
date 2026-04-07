c86103198b11a135eda71a49d3bb1d87eeb5a8b3
Aditya <adithya.aneesh@student.saedubai.com>
2026-04-07 20:09:00 +0400

    added beat pattern and modified beat collider size

--- Assets/_Main/1_Scripts/SpawnManager/SpawnManager.cs
+++ Assets/_Main/1_Scripts/SpawnManager/SpawnManager.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
+
+
 
 public class SpawnManager : ScriptLibrary.Singletons.Singleton<SpawnManager>
 {
-    public Action OnSpawn;
+    public event Action OnSpawn;
     public float bpm = 124f;
     public float startDelay = 2.0f;
     public float travelTime = 2.438f;
@@ -15,11 +18,16 @@ public class SpawnManager : ScriptLibrary.Singletons.Singleton<SpawnManager>
     private double _nextBeatTime;
     private float _secondsPerBeat;
     private bool _songStarted = false;
+    
+    // queue containing all the beat flags, if its true, its played, if its false its not
+    private Queue<bool> _beatQueue = new Queue<bool>();
 
     void Start()
     {
         _musicSource = GetComponent<AudioSource>();
         _secondsPerBeat = 60f/bpm;
+
+        LoadBeatsFromJSON("beatpattern");
 
         _songStartTime = AudioSettings.dspTime + startDelay;
         _nextBeatTime = _songStartTime;
@@ -28,23 +36,71 @@ public class SpawnManager : ScriptLibrary.Singletons.Singleton<SpawnManager>
         _songStarted = true;
     }
 
-    private int _beatIndex = 0;
+    private void LoadBeatsFromJSON(string fileName)
+    {
+        TextAsset jsonFile = Resources.Load<TextAsset>(fileName);
+        
+        if (jsonFile == null)
+        {
+            Debug.LogError($"Couldn't load {fileName}");
+            return;
+        }
+
+        try
+        {
+            //convert json to array in beatpattern class
+            BeatPattern pattern = JsonUtility.FromJson<BeatPattern>(jsonFile.text);
+            
+            //checking if beats is null or if its empty
+            if (pattern?.beats == null || pattern.beats.Length == 0)
+            {
+                Debug.LogError("Beat pattern is empty!");
+                return;
+            }
+
+            //add the beats(array of bool) to local queue of bool
+            for (int i = 0; i < pattern.beats.Length; i++)
+            {
+                _beatQueue.Enqueue(pattern.beats[i]);
+            }
+
+            Debug.Log($"loaded {_beatQueue.Count} beats from {fileName}");
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"parsing error: {e.Message}");
+        }
+    }
+
     void Update()
     {
         if (!_songStarted) return;
 
         if (AudioSettings.dspTime >= _nextBeatTime - travelTime)
         {
-            if (_beatIndex % 4 == 0)
-                OnSpawn?.Invoke(); 
-            _beatIndex++;
+            //check if beat should be played from queee
+            bool shouldPlayBeat = _beatQueue.Count <= 0 || _beatQueue.Dequeue();
+            
+            print(shouldPlayBeat);
+            if (shouldPlayBeat)
+            {
+                OnSpawn?.Invoke();
+            }
+            
             _nextBeatTime += _secondsPerBeat;
         }
+
     }
     public float GetNoteSpeed(Vector3 spawnPos, Vector3 hitPos)
     {
         float distance = Vector3.Distance(spawnPos, hitPos);
         return distance / travelTime;
     }
+}
+
+[Serializable]
+public class BeatPattern
+{
+    public bool[] beats;
 }
 
